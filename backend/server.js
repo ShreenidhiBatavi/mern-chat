fa1f777da1437b5980e1798dd1ec065799084943
Shreenidhi Batavi <81751036+ShreenidhiBatavi@users.noreply.github.com>
2024-08-12 14:23:10 +0530

    adding main entry file for server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const { chats } = require("./data/data");
+dotenv.config();
+connectDB();
 const app = express();
-app.listen(5000, () => console.log("server started at port 5000"));
+
+app.get("/", (req, res) => {
+  res.send("shri hare krishna!");
+});
+app.get("/api/chat", (req, res) => {
+  console.log(chats);
+  res.send(chats);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
