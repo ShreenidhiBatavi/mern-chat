aee7c30cc7647c1de3c4eee5a42556adf5d62879
Shreenidhi Batavi <81751036+ShreenidhiBatavi@users.noreply.github.com>
2024-08-17 17:17:17 +0530

    removing intial test api end points

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,5 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("shri hare krishna!");
-});
-app.get("/api/chat", (req, res) => {
-  console.log(chats);
-  res.send(chats);
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
