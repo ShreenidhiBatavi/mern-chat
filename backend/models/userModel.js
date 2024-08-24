99ba4180fe8a152f74a70a6d3e80450dc36af977
Shreenidhi Batavi <81751036+ShreenidhiBatavi@users.noreply.github.com>
2024-08-24 17:34:13 +0530

    changes in user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
       default: "https://via.placeholder.com/150x150",
     },
   },
@@ -15,5 +15,17 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-const User = mongoose.model("User", messageModel);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
