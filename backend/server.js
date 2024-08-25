ab8c58440a3abf4878c38dcad6fcf1ce1e90dd20
Shreenidhi Batavi <81751036+ShreenidhiBatavi@users.noreply.github.com>
2024-08-25 23:31:09 +0530

    adding user route

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const { chats } = require("./data/data");
 dotenv.config();
+const userRoutes = require("./routes/userRoutes");
 connectDB();
 const app = express();
+app.use(express.json());
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
